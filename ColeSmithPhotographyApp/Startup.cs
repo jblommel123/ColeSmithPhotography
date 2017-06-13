39151f584cc07aab18a9846c0d73829f44fe73bd
Joe Blommel <blommel17@gmail.com>
2017-06-12 22:40:49 -0400

    created project

---
+++ ColeSmithPhotographyApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ColeSmithPhotographyApp.Startup))]
+namespace ColeSmithPhotographyApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
